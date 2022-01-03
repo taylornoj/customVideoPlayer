afd68910a33978b04ea6c4c2004745fa9f9d0cf8
Taylor Noj <taylornoj@gmail.com>
2022-01-03 05:05:57 +0000

    add function scrub to calculate percentage of scrubtime by video duration and listen for click event

--- scripts.js
+++ scripts.js
@@ -46,6 +46,11 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 // -------TODO: hook up event listeners
 
 // click on video to play and pause
@@ -61,3 +66,4 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+progress.addEventListener('click', scrub);
