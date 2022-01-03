997cabe39d6342d300b1c105983330e81e630a60
Taylor Noj <taylornoj@gmail.com>
2022-01-03 04:04:30 +0000

    add mousedown event listener to gain continuous value on slide

--- scripts.js
+++ scripts.js
@@ -49,3 +49,4 @@ video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
